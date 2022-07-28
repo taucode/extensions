5e0dedd691ca6eacd9939f4b44945e6e1027fe45
Andrey Kovalenko <ak@taucode.com>
2022-07-29 02:37:39 +0300

    Ready for next dev cycle (1.3.8).

--- dev-build-release.bat
+++ dev-build-release.bat
@@ -1,3 +1,9 @@
 dotnet restore
+
+dotnet build --configuration Debug
 dotnet build --configuration Release
+
+dotnet test -c Debug .\test\TauCode.Extensions.Tests\TauCode.Extensions.Tests.csproj
+dotnet test -c Release .\test\TauCode.Extensions.Tests\TauCode.Extensions.Tests.csproj
+
 nuget pack nuget\TauCode.Extensions.nuspec
