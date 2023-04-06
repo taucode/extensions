465110355b463b5e520f5da0a5695ed938e36642
Andrey Kovalenko <ak@taucode.com>
2023-04-06 14:07:49 +0300

    Ready to release 1.8.1 alpha.

--- src/TauCode.Extensions/CollectionExtensions.cs
+++ src/TauCode.Extensions/CollectionExtensions.cs
@@ -252,4 +252,36 @@ public static class CollectionExtensions
 
         return true;
     }
+
+    public static void AddMany<T>(this IEnumerable<T> target, IEnumerable<T> source)
+    {
+        if (target == null!)
+        {
+            throw new ArgumentNullException(nameof(target));
+        }
+
+        if (source == null!)
+        {
+            throw new ArgumentNullException(nameof(source));
+        }
+
+        switch (target)
+        {
+            case List<T> list:
+                list.AddRange(source);
+                return;
+
+            case ICollection<T> collection:
+                {
+                    foreach (var v in source)
+                    {
+                        collection.Add(v);
+                    }
+
+                    break;
+                }
+        }
+
+        throw new ArgumentException("Collection does not support adding.", nameof(target));
+    }
 }
